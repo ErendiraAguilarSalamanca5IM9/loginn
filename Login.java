7af20fe1a57e15ae9a05d981f099c01d7487a686
ErendiraAguilarSalamanca5IM9 <42345711+ErendiraAguilarSalamanca5IM9@users.noreply.github.com>
2018-09-25 21:17:31 -0500

    Add files via upload
    @emmanuel789

---
+++ Login.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }}
+     public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+         String contra;
+         String nombre;
+        
+      //recuperamos los datos del formulario
+          nombre = request.getParameter("username");
+          contra = request.getParameter("pass");
+         
+      HttpSession sesion = request.getSession();
+      sesion.setAttribute("claveSesion", nombre + contra);
+
+       response.setContentType("text/html");
+      //Mostramos los  valores en el cliente
+      PrintWriter out = response.getWriter();
+       
+      out.println("<a href=\"/Adopciones/catalogo.jsp\"> Link al catalogo del carrito  </a>");
+      out.println("<br>");
+      out.println("ID de la sesi&oacute;n: " + sesion.getId());
+  
+    }
+   
+
+}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+ 
+
+
